a38b2599f2f432bcc7b23f02a6de6c48f7b94f14
PaoloPascucci <pascuccip9@gmail.com>
2023-10-19 17:52:47 +0200

    add difficult at start

--- js/start.js
+++ js/start.js
@@ -2,21 +2,21 @@ const inputCheck = document.getElementById("check");
 const linkButton = document.getElementById("proceedButton");
 const radioButtonEasy = document.getElementById("easy");
 const radioButtonHard = document.getElementById("hard");
-console.log("easy:", radioButtonEasy);
-console.log("hard:", radioButtonHard);
+
 const hardValue = radioButtonHard.parentElement.innerText.toLowerCase();
 const easyValue = radioButtonEasy.parentElement.innerText.toLowerCase();
 // console.log(hardValue);
 
 function toggle() {
   if (inputCheck.checked && radioButtonHard.checked) {
-    linkButton.setAttribute = ("href", "./questions.html?difficoltà=hard");
+    linkButton.href = "./questions.html?difficoltà=hard";
     console.log(linkButton);
   } else if (inputCheck.checked && radioButtonEasy.checked) {
-    linkButton.setAttribute = ("href", "./questions.html?difficoltà=easy");
+    inkButton.href = "./questions.html?difficoltà=easy";
     console.log(linkButton);
   } else {
-    linkButton.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    inkButton.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    
     console.log(linkButton);
   }
 }
