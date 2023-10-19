f4c66e91bad69a06031da92cbd1baf6466ec478f
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-10-19 15:32:46 +0200

    homepage with difficulty

--- js/share.js
+++ js/share.js
@@ -9,11 +9,23 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit",
-    ],
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What port does HTTP run on?",
+    correct_answer: "80",
+    incorrect_answers: ["53", "443", "23"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Which of these names was an actual codename for a cancelled Microsoft project?",
+    correct_answer: "Neptune",
+    incorrect_answers: ["Enceladus", "Pollux", "Saturn"],
   },
   {
     category: "Science: Computers",
@@ -35,9 +47,33 @@ const questions = [
   {
     category: "Science: Computers",
     type: "boolean",
-    difficulty: "easy",
+    difficulty: "hard",
+    question: "DHCP stands for Dynamic Host Configuration Port.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
     question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
+      "What is the name of the process that sends one qubit of information using two bits of classical information?",
+    correct_answer: "Quantum Teleportation",
+    incorrect_answers: ["Super Dense Coding", "Quantum Entanglement", "Quantum Programming"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "America Online (AOL) started out as which of these online service providers?",
+    correct_answer: "Quantum Link",
+    incorrect_answers: ["CompuServe", "Prodigy", "GEnie"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -45,8 +81,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -56,20 +91,32 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet",
-    ],
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
+    difficulty: "hard",
+    question: "Who invented the &quot;Spanning Tree Protocol&quot;?",
+    correct_answer: "Radia Perlman",
+    incorrect_answers: ["Paul Vixie", "Vint Cerf", "Michael Roberts"],
+  },
+
+  {
+    category: "Science: Computers",
+    type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
+    question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "The acronym &quot;RIP&quot; stands for which of these?",
+    correct_answer: "Routing Information Protocol",
+    incorrect_answers: ["Runtime Instance Processes", "Regular Interval Processes", "Routine Inspection Protocol"],
   },
   {
     category: "Science: Computers",
@@ -79,6 +126,15 @@ const questions = [
     correct_answer: "140",
     incorrect_answers: ["120", "160", "100"],
   },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What was the name of the first Bulgarian personal computer?",
+    correct_answer: "IMKO-1",
+    incorrect_answers: ["Pravetz 82", "Pravetz 8D", "IZOT 1030"],
+  },
+
   {
     category: "Science: Computers",
     type: "boolean",
@@ -91,9 +147,25 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
+    question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "What type of sound chip does the Super Nintendo Entertainment System (SNES) have?",
+    correct_answer: "ADPCM Sampler",
+    incorrect_answers: ["FM Synthesizer", "Programmable Sound Generator", "PCM Sampler"],
+  },
+
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "hard",
+    question: "Who is the founder of Palantir?",
+    correct_answer: "Peter Thiel",
+    incorrect_answers: ["Mark Zuckerberg", "Marc Benioff", "Jack Dorsey"],
   },
 ];
